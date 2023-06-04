73028079adc9653a6d0ad8906275d9e1cb26aece
Gurkirat Singh Arora <sgurkirat868@gmail.com>
2023-06-04 21:37:33 +0530

    code refactoring

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -8,9 +8,6 @@ const initialState = {
 }
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  // const [currentOperand, setCurrentOperand] = useState('');
-  // const [previousOperand, setPreviousOperand] = useState('');
-  // const [operator, setOperator] = useState('');
 
   const onClickSetOperator = (e) => {
     if (state.operator !== '' && state.currentOperand === '' && state.previousOperand !== '') {
@@ -18,77 +15,23 @@ function App() {
     }
     else if (state.operator !== '' && state.currentOperand !== '' && state.previousOperand !== '') {
       dispatch({ type: 'OPERATION_ON_CHANGE_OPERATOR', payload: e.target.value })
-
-      // const prev = Number.parseFloat(previousOperand);
-      // const curr = Number.parseFloat(currentOperand);
-
-      // if (operator === '/') {
-      //   setPreviousOperand(prev / curr);
-      // }
-      // else if (operator === 'X') {
-      //   setPreviousOperand(prev * curr);
-      // }
-      // else if (operator === '+') {
-      //   setPreviousOperand(prev + curr);
-      // }
-      // else if (operator === '-') {
-      //   setPreviousOperand(prev - curr);
-      // }
-      // setOperator(e.target.value);
-      // console.log(currentOperand);
-      // setCurrentOperand('');
     }
     else {
       dispatch({ type: "SET_OPERATOR", payload: e.target.value })
-      // setPreviousOperand(currentOperand);
-      // setCurrentOperand('');
-      // setOperator(e.target.value);
     }
   }
   const onClickSetOperand = (e) => {
     dispatch({ type: 'CONCATENATING_CURRENT_OPERAND', payload: e.target.value })
-    // setCurrentOperand((prev) => {
-    //   return prev + e.target.value;
-    // })
   }
   const clearLastEntry = () => {
     dispatch({ type: "DELETING_LAST_ENTRY" });
-    // setCurrentOperand((prev) => {
-    //   let str;
-    //   let temp;
-    //   str = prev.toString();
-    //   if (str.length !== 0) {
-    //     temp = str.slice(0, str.length - 1);
-    //     return temp;
-    //   }
-    // })
   }
   const deleteAll = () => {
     dispatch({ type: "DELETE_ALL" });
-
-    // setCurrentOperand('');
-    // setOperator('');
-    // setPreviousOperand('');
   }
   const setResult = () => {
     if (state.previousOperand !== '' && state.currentOperand !== '' && state.operator !== '') {
       dispatch({ type: "DISPLAY_RESULT" })
-      // const prev = Number.parseFloat(previousOperand);
-      // const curr = Number.parseFloat(currentOperand);
-      // if (operator === '/') {
-      //   setCurrentOperand(prev / curr);
-      // }
-      // else if (operator === 'X') {
-      //   setCurrentOperand(prev * curr);
-      // }
-      // else if (operator === '+') {
-      //   setCurrentOperand(prev + curr);
-      // }
-      // else if (operator === '-') {
-      //   setCurrentOperand(prev - curr);
-      // }
-      // setPreviousOperand('');
-      // setOperator('')
     }
   }
   return (
